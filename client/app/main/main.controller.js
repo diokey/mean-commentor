3b312b81808bcb10abfb9689baa9eb8c32231ce3
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-01-27 22:45:11 -0500

    Hooking client to the server

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,31 @@
 'use strict';
 
 angular.module('meanCommentorApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('MainCtrl', function ($scope, $http, socket) {
+    $scope.newComment = '';
+
+    $http.get('/api/comments').success(function(comments) {
+        $scope.comments = comments;
+
+        socket.syncUpdates('comment',$scope.comments, function(){
+            comments.sort(function(a,b) {
+                a = new Date(a.date);
+                b = new Date(b.date);
+                return a > b ? - 1 : a < b ? 1 : 0;
+            }); 
+        });
+    });
+   
+    //clelan up listeneres when controller is destroyed
+    $scope.$on('$destroy',function(){
+        socket.unsyncUpdates('comment');
+    });
+
+    //use our rest api to post new Comments
+    $scope.addComment = function () {
+       $http.post('/api/comments',{content : $scope.newComment});
+
+       $scope.newComment = '';
+    };
+
   });
